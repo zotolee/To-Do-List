3d538e63599ef8f6c2087343016feb3d1774a2b8
zotolee <akinayo711@gmail.com>
2022-02-20 13:38:16 -0800

    saving to github

---
+++ app.js
@@ -0,0 +1,8 @@
+//Selectors
+const TodoButton = document.querySelector('.todo-btn');
+const TodoInput = document.querySelector('.todo-input');
+const TodoList = document.querySelector('.todo-list');
+
+//EventListeners
+
+//Functions
