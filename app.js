73e5506dc6b008e2e301e583c5f01354953c8f9b
zotolee <akinayo711@gmail.com>
2022-02-24 14:06:22 -0800

    saving to github

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ const TodoInput = document.querySelector('.todo-input');
 const TodoList = document.querySelector('.todo-list');
 
 //EventListeners
-
+TodoButton.addEventListener("click", addTodo);
 //Functions
+function addTodo(){
+    //prevent form from submitting
+    event.preventDefault();
+    //creating div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    //creating LI
+    
+}
